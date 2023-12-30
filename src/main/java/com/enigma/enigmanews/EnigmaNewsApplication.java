5a0ee1bd13fb8605267e9df26b37d8caee23f593
saputra khotim <saputra.khotim108@gmail.com>
2023-12-30 11:34:33 +0700

    commit initial

---
+++ src/main/java/com/enigma/enigmanews/EnigmaNewsApplication.java
@@ -0,0 +1,13 @@
+package com.enigma.enigmanews;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnigmaNewsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnigmaNewsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/enigma/enigmanews/EnigmaNewsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enigma.enigmanews;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnigmaNewsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
